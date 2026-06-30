30a47d2328a62f9f3d459eb35f1b8a2f993b46e6
Behdad Tabrizi <b.tabrizi@deroso.de>
2026-06-30 14:18:04 +0200

    [Update] db path

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "bcryptjs"],
+  outputFileTracingIncludes: {
+    "/**": ["./prisma/dev.db"],
+  },
 };
 
 export default withNextIntl(nextConfig);
